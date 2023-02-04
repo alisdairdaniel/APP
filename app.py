1c627de1739840bc8f81effd2ad104576e3b1e2d
alisdairdaniel <117979215+alisdairdaniel@users.noreply.github.com>
2023-02-04 00:17:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ def hello_world():
     return '''It s my pleasure to introduce you to koyeb, who will make the process of building programs easy for you.
     Here is the official website 'www.koyeb.com',they provide free service.'''
 
-
 if __name__ == "__main__":
     app.run()
 # TODO Next will be combined with HTML to achieve a complete web page.
